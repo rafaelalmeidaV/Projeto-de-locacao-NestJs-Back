dcb33ff0d4ef870cea84a62c9d35f64f6f777556
Rafael Almeida V <104868359+rafaelalmeidaV@users.noreply.github.com>
2023-12-06 15:14:19 -0300

    feat/Cliente (HTTP: Post and Get)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,13 @@ import { ImovelController } from './imovel/imovel.controller';
 import { ImovelModule } from './imovel/imovel.module';
 import { ImovelService } from './imovel/imovel.service';
 import { PrismaService } from './database/prisma.service';
+import { ClienteController } from './cliente/cliente.controller';
+import { ClienteService } from './cliente/cliente.service';
+import { ClienteModule } from './cliente/cliente.module';
 
 @Module({
-  imports: [ImovelModule],
-  controllers: [AppController, ImovelController],
-  providers: [AppService, ImovelService, PrismaService],
+  imports: [ImovelModule, ClienteModule],
+  controllers: [AppController, ImovelController, ClienteController],
+  providers: [AppService, ImovelService, PrismaService, ClienteService],
 })
 export class AppModule {}
---
+++ src/cliente/cliente.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ClienteController } from './cliente.controller';
+
+describe('ClienteController', () => {
+  let controller: ClienteController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ClienteController],
+    }).compile();
+
+    controller = module.get<ClienteController>(ClienteController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
