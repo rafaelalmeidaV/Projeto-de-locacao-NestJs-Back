4c8c3e9118ff3ada1a4b050693a915641d532f31
Rafael Almeida V <104868359+rafaelalmeidaV@users.noreply.github.com>
2023-12-06 15:03:33 -0300

    feat/Imovel (HTTP: Post And Get)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ImovelController } from './imovel/imovel.controller';
+import { ImovelModule } from './imovel/imovel.module';
+import { ImovelService } from './imovel/imovel.service';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ImovelModule],
+  controllers: [AppController, ImovelController],
+  providers: [AppService, ImovelService, PrismaService],
 })
 export class AppModule {}
---
+++ src/imovel/imovel.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ImovelController } from './imovel.controller';
+
+describe('ImovelController', () => {
+  let controller: ImovelController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ImovelController],
+    }).compile();
+
+    controller = module.get<ImovelController>(ImovelController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
